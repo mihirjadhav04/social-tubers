c516a1d4985bfd498c77d8de5cffb987af6149af
heena-8780 <62840141+heena-8780@users.noreply.github.com>
2022-03-13 17:05:57 +0530

    modified urls.py

--- website/influencers/urls.py
+++ website/influencers/urls.py
@@ -6,5 +6,5 @@ from .views import influencers, influencer_details
 
 urlpatterns = [
     path("influencers/", influencers, name="influencers"),
-    path("<int:id>", influencer_details, name="influencer_details"),
+    # path("<int:id>", influencer_details, name="influencer_details"),
 ]
