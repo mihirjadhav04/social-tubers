2c2ebf33a630dc8b0c8b5b5366af57a633809166
MihirJadhav00 <mihireducation02@gmail.com>
2022-05-09 17:59:34 +0530

    DB changed to postgres

--- website/settings.py
+++ website/settings.py
@@ -68,23 +68,23 @@ WSGI_APPLICATION = "website.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 import dj_database_url
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "socialtubers_db",
-#         "USER": "postgres",
-#         "PASSWORD": "apple@143",
-#         "HOST": "",
-#         "PORT": "",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "socialtubers_db",
+        "USER": "postgres",
+        "PASSWORD": "apple@143",
+        "HOST": "",
+        "PORT": "",
+    }
+}
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
