6b0fae87f4d4d527d7af3088e8b27cb3c5510da3
MihirJadhav00 <mihireducation02@gmail.com>
2022-05-12 12:19:16 +0530

    Made influencer recommendation worked

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -7,18 +7,18 @@ if __name__ == "__main__":
 
     def run():
 
-        # Influencer.objects.all().delete()
+        # User.objects.all().delete()
 
-        with open('./apnawala.csv', encoding='cp437') as file:
-            reader = csv.reader(file)
-            next(reader)  # Advance past the header
-            for row in reader:
-                user = User(name=row[0],email=row[7],is_influencer=True)
-                user.save()
-                influencer = Influencer(user=user,channel_name=row[6],youtube_id=row[10],category_type=row[8],short_description=row[9])
-                influencer.profile_photo.save('temp.png', File(open('C:/Users/Lenovo/OneDrive/Desktop/Ronak/LYP-main2/website/influencers/download.png', 'rb')))
-                influencer.save()
+with open('C:/Users/Mihir Jadhav/Downloads/apnawala123.csv', encoding='cp437') as file:
+    reader = csv.reader(file)
+    next(reader)  # Advance past the header
+    for row in reader:
+        user = User(name=row[0],email=row[7],is_influencer=True)
+        user.save()
+        influencer = Influencer(user=user,channel_name=row[6],youtube_id=row[10],category_type=row[8],short_description=row[9])
+        influencer.profile_photo.save('temp.jpg', File(open('C:/Users/Mihir Jadhav/Desktop/LYP/media/influencer_images/2022/04/me.jpg', 'rb')))
+        influencer.save()
 
-        print("done")
+print("done")
 
     run()
